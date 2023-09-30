c5da3902bd92ad26c3cc11279fa0cb836c486f22
Shravan <newshravank25@gmail.com>
2023-09-30 11:07:43 +0530

    all the necessary webpages added, admin functionality added

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import DbForm
 
-# Register your models here.
+class DbFormAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email")
+    search_fields = ("first_name", "last_name", "email")
+    list_filter = ("date", "occupation")
+    ordering = ("first_name",)
+    readonly_fields = ("occupation",)
+
+admin.site.register(DbForm, DbFormAdmin)
+
--- job_application/views.py
+++ job_application/views.py
@@ -1,5 +1,37 @@
 from django.shortcuts import render
+from .forms import ApplicationForm
+from .models import DbForm
+from django.contrib import messages
+from django.core.mail import EmailMessage
 
 
 def index(request):
+    if request.method == "POST":
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            date = form.cleaned_data["date"]
+            occupation = form.cleaned_data["occupation"]
+
+            DbForm.objects.create(first_name=first_name,
+                                  last_name=last_name,
+                                  email=email, date=date,
+                                  occupation=occupation)
+
+            message_body = f"A new job application was submitted, Thank you {first_name} {last_name}"
+            email_message = EmailMessage("Application Confirmation", message_body, to=[email])
+            email_message.send()
+
+            messages.success(request, "Form Submitted Successfully, We will reach out!")
+
     return render(request, "index.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
